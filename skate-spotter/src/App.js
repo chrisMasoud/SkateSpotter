025be84439c68dae6673a249d3365ed8b54f0f5a
chrisMasoud <chrismasoud22@gmail.com>
2023-06-08 18:12:32 -0400

    Created LoginButton component

--- skate-spotter/src/App.js
+++ skate-spotter/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import SearchBar from "./components/SearchBar";
+import LoginButton from "./components/LoginButton";
 
 function App() {
   return (
     <div>
       <header>
         <SearchBar />
+        <LoginButton />
       </header>
     </div>
   );
