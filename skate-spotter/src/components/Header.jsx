d9ba82e5421e0c950f3569178ba0d884bb2a3c6d
chrisMasoud <chrismasoud22@gmail.com>
2023-06-09 14:49:12 -0400

    Added Logo component

--- skate-spotter/src/components/Header.jsx
+++ skate-spotter/src/components/Header.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
+import Logo from "./Logo";
 
 function Header() {
   return (
     <header>
+      <Logo />
       <SearchBar />
       <LoginButton />
       <LogoutButton />
---
+++ skate-spotter/src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <div>
+      <img src="SkateSpotter-logo.png" alt="SkateSpotter Logo" />
+    </div>
+  );
+}
+
+export default Logo;
