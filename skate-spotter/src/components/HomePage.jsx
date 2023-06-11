cbca5fd82dfee6aa487e24c556550931d76276ee
chrisMasoud <chrismasoud22@gmail.com>
2023-06-11 16:20:41 -0400

    Created HomePage

---
+++ skate-spotter/src/components/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import CardList from "./CardList";
+import Map from "./Map";
+
+export default function TipsPage() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <Map />
+      <CardList />
+    </>
+  );
+}
