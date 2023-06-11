f8c70831b0ecef9061c4f2b1593af95f7598bc2f
chrisMasoud <chrismasoud22@gmail.com>
2023-06-10 20:23:20 -0400

    Added Navbar component

--- skate-spotter/src/App.js
+++ skate-spotter/src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import Header from "./components/Header";
 import SpotCardList from "./components/SpotCardList";
 import Map from "./components/Map";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <Header />
+      <Navbar />
       <Map />
       <SpotCardList />
     </>
---
+++ skate-spotter/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AddSpotButton from "./AddSpotButton";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <AddSpotButton />
+      <AddSpotButton />
+      <AddSpotButton />
+      <AddSpotButton />
+      <AddSpotButton />
+    </nav>
+  );
+}
