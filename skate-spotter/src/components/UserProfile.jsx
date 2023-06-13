aa8833ac8d03542ee0bde627a19d0b83785696e4
chrisMasoud <chrismasoud22@gmail.com>
2023-06-13 11:14:59 -0400

    Profile pic upload button functionality

--- skate-spotter/src/components/UserProfile.jsx
+++ skate-spotter/src/components/UserProfile.jsx
@@ -2,18 +2,28 @@ import React from "react";
 import UserAvatar from "./UserAvatar";
 import CardList from "./CardList";
 import ProfilePicUploadButton from "./ProfilePicUploadButton";
+import { useState } from "react";
 
 export default function UserProfile() {
-  const imageUrl = "https://randomuser.me/api/portraits/men/1.jpg";
+  const defaultImageUrl = "https://randomuser.me/api/portraits/men/1.jpg";
   const avatarSize = 150;
+  const [imageUrl, setImageUrl] = useState(defaultImageUrl);
+
+  const handleImageSelect = (file) => {
+    const reader = new FileReader();
+    reader.onload = (event) => {
+      setImageUrl(event.target.result);
+    };
+    reader.readAsDataURL(file);
+  };
 
   return (
     <section className="profile">
       <div className="profileForm">
         <UserAvatar imageUrl={imageUrl} size={avatarSize} />
-        <ProfilePicUploadButton />
-        <h1 style={{ marginLeft: "190px", marginTop: "10px" }}>User Name</h1>
-        <h4 style={{ marginLeft: "220px", marginTop: "-10px" }}>Zip Code</h4>
+        <ProfilePicUploadButton onImageSelect={handleImageSelect} />
+        <h1 style={{ marginTop: "20px" }}>User Name</h1>
+        <h4 style={{ marginTop: "-10px" }}>Zip Code</h4>
         <textarea
           className="profileBio"
           placeholder="Enter your biography..."
