b2fca2bb0bc96563f4610896118701b21d2b2998
chrisMasoud <chrismasoud22@gmail.com>
2023-06-13 11:26:00 -0400

    Updated news and tips cards to open external links

--- skate-spotter/src/components/TipsPage.jsx
+++ skate-spotter/src/components/TipsPage.jsx
@@ -1,12 +1,20 @@
 import React from "react";
-import CardList from "./CardList";
+import LinkCard from "./LinkCard";
 import TipsPageHeader from "./TipsPageHeader";
 
 export default function TipsPage() {
+  const cardTitle = "Card Name";
+  const cardUrl = "https://www.youtube.com/watch?v=4XgjDhSnTtE";
+
   return (
     <>
       <TipsPageHeader />
-      <CardList />
+      <section className="spotCardSection">
+        <LinkCard title={cardTitle} url={cardUrl} />
+        <LinkCard title={cardTitle} url={cardUrl} />
+        <LinkCard title={cardTitle} url={cardUrl} />
+        <LinkCard title={cardTitle} url={cardUrl} />
+      </section>
     </>
   );
 }
