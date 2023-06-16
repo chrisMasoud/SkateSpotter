51281e9384e5c42fc30a1ca560f0f2cc414b0069
chrisMasoud <chrismasoud22@gmail.com>
2023-06-16 18:26:54 -0400

    Made pins clickable and link to detail page

--- skate-spotter/src/components/Map.jsx
+++ skate-spotter/src/components/Map.jsx
@@ -1,6 +1,12 @@
 import React from "react";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
 import key from "../key.json";
+import { useNavigate } from "react-router-dom";
 
 const containerStyle = {
   width: "1500px",
@@ -16,6 +22,8 @@ const defualtCenter = {
 
 function Map({ center, spots }) {
   const [map, setMap] = React.useState(null);
+  const [selectedSpot, setSelectedSpot] = React.useState(null);
+  const navigate = useNavigate();
   const api_key = key.apikey;
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
@@ -29,6 +37,14 @@ function Map({ center, spots }) {
   const onUnmount = React.useCallback(function callback(map) {
     setMap(null);
   }, []);
+
+  const handleMarkerClick = (spot) => {
+    setSelectedSpot(spot);
+  };
+
+  const handleCloseInfoWindow = () => {
+    setSelectedSpot(null);
+  };
 
   return isLoaded ? (
     <div className="mapContainer">
@@ -48,8 +64,29 @@ function Map({ center, spots }) {
               lng: parseFloat(spot.Longitude),
             }}
             title={spot.SpotName}
+            onClick={() => handleMarkerClick(spot)}
           />
         ))}
+        {selectedSpot && (
+          <InfoWindow
+            position={{
+              lat: parseFloat(selectedSpot.Latitude),
+              lng: parseFloat(selectedSpot.Longitude),
+            }}
+            onCloseClick={handleCloseInfoWindow}
+          >
+            <div>
+              <h3>{selectedSpot.SpotName}</h3>
+              <button
+                onClick={() =>
+                  navigate(`/page`, { state: { data: selectedSpot } })
+                }
+              >
+                View Details
+              </button>
+            </div>
+          </InfoWindow>
+        )}
       </GoogleMap>
     </div>
   ) : (
