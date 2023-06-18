164709aee0733ec490b645081c6601628b1046ff
chrisMasoud <chrismasoud22@gmail.com>
2023-06-18 14:34:16 -0400

    Styled review cards

--- skate-spotter/src/components/ReviewCard.jsx
+++ skate-spotter/src/components/ReviewCard.jsx
@@ -2,12 +2,17 @@ import React from "react";
 
 function ReviewCard() {
   return (
-    <div className="card">
+    <div className="reviewCard">
       <div className="textBox">
         <p className="h1">Review Title</p>
-        <p>Review Text...</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit
+        </p>
         <p>Rating: </p>
-        <p>- UserName</p>
       </div>
     </div>
   );
