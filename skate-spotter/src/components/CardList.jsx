ab13f9ee4bbc42534dfbc45d813294c81b454df5
bsilv22 <bsilv22@yahoo.com>
2023-06-25 14:03:27 -0400

    Updating

---
+++ skate-spotter/src/components/CardList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Card from "./Card";
+
+function CardList() {
+  return (
+    <section className="spotCardSection">
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+      <Card title="Card Name" />
+    </section>
+  );
+}
+
+export default CardList;
---
+++ skate-spotter/src/components/SpotCardList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SpotCard from "./SpotCard";
+
+function SpotCardList() {
+  return (
+    <main>
+      <SpotCard />
+      <SpotCard />
+    </main>
+  );
+}
+
+export default SpotCardList;
