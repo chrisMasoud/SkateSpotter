e31dbae0eb696c2a521144a7a575ce3c06d370ec
bsilv22 <bsilv22@yahoo.com>
2023-06-25 18:30:33 -0400

    Added an improved footer

--- skate-spotter/src/components/Footer.jsx
+++ skate-spotter/src/components/Footer.jsx
@@ -2,9 +2,26 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    
-    <footer style={{ textAlign: 'center', padding: '20px', backgroundColor: 'lightgray' }}>
-      <p style={{ fontSize: '20px', margin: '0 auto' }}>This is the footer.</p>
+    <footer style={{ backgroundColor: '#f2f2f2', padding: '20px', textAlign: 'center' }}>
+      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+        <a href="/about">About Us</a>
+        <span style={{ margin: '0 10px' }}>|</span>
+        <a href="/contact">Contact Me</a>
+      </div>
+      <div>
+        <a href="https://www.facebook.com/example" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-facebook"></i>
+        </a>
+        <a href="https://www.twitter.com/example" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-twitter"></i>
+        </a>
+        <a href="https://www.linkedin.com/in/example" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a href="https://www.instagram.com/example" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
     </footer>
   );
 };
