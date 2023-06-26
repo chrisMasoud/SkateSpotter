34e701c0dd5c7d340995213b33edbf479c693e8a
chrisMasoud <chrismasoud22@gmail.com>
2023-06-26 11:07:40 -0400

    Added SupportButton to Footer

--- skate-spotter/src/components/Footer.jsx
+++ skate-spotter/src/components/Footer.jsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React from "react";
+import SupportButton from "./SupportButton";
 
 const Footer = () => {
   return (
-    <footer style={{ backgroundColor: '#f2f2f2', padding: '20px', textAlign: 'center' }}>
-      <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+    <footer
+      style={{
+        backgroundColor: "#f2f2f2",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginBottom: "20px",
+        }}
+      >
         <a href="/about">About Us</a>
-        <span style={{ margin: '0 10px' }}>|</span>
-        <a href="/contact">Contact Me</a>
+        <span style={{ margin: "0 10px" }}>|</span>
+        <SupportButton />
       </div>
       <div>
-        <a href="https://www.facebook.com/example" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.facebook.com/example"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-facebook"></i>
         </a>
-        <a href="https://www.twitter.com/example" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.twitter.com/example"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-twitter"></i>
         </a>
-        <a href="https://www.linkedin.com/in/example" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.linkedin.com/in/example"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-linkedin"></i>
         </a>
-        <a href="https://www.instagram.com/example" target="_blank" rel="noopener noreferrer">
+        <a
+          href="https://www.instagram.com/example"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           <i className="fab fa-instagram"></i>
         </a>
       </div>
